f9cb68fec686714e95d7ad188a190f73c02dc9fc
Sean Conner <spc@conman.org>
2014-08-25 16:55:05 -0400

    The Lua script that uses the buffer module.

---
+++ suckb.lua
@@ -0,0 +1,51 @@
+#!/usr/bin/env lua
+
+errno  = require "org.conman.errno"
+signal = require "org.conman.signal-ansi"
+getopt = require "org.conman.getopt"
+net    = require "net"
+buffer = require "buffer"
+
+OPTS = 
+{
+  { "a" , "" , true  , function(a) HOST = a end },
+  { "p" , "" , true  , function(p) PORT = tonumber(p) end },
+  { "s" , "" , true  , function(s) SCRIPT = s end } ,
+  { "h" , "" , false , function()
+      io.stderr:write("usage: ",arg[0]," -a addr -p port -s script [-h]\n")
+      os.exit(1)
+    end
+  },
+}
+
+if #arg == 0 then
+  io.stderr:write("usage: ",arg[0]," -a addr -p port -s script [-h]\n")
+  os.exit(1)
+end
+
+getopt.getopt(arg,OPTS)
+
+dofile(SCRIPT)
+
+addr = net.address(HOST,'udp',PORT)
+sock = net.socket(addr.family,'udp')
+sock:bind(addr)
+packet_count = 0
+
+signal.catch('int',function()
+  print(packet_count)
+  print(math.floor(collectgarbage('count')))
+  os.exit(0)
+end)
+
+data = buffer.new()
+
+while true do
+  local remote,err = sock:recvb(data)
+  if err ~= 0 then
+    io.stderr:write("sock:recv(): ",errno[err])
+  else
+    packet_count = packet_count + 1
+    main(data)
+  end
+end
